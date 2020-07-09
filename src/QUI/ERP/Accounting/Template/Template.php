09f17073a18f7e91cf2b699b549e492fc615f8ca
Patrick Müller <p.mueller@pcsg.de>
2020-07-09 12:08:23 +0200

    refactor: OutputTemplateProvider -> changed some methods to reflect new interface

--- src/QUI/ERP/Accounting/Template/Template.php
+++ src/QUI/ERP/Accounting/Template/Template.php
@@ -5,6 +5,7 @@ namespace QUI\ERP\Accounting\Template;
 use QUI;
 use QUI\ERP\Output\OutputTemplateProviderInterface;
 use QUI\Interfaces\Template\EngineInterface;
+use QUI\Locale;
 
 class Template implements OutputTemplateProviderInterface
 {
@@ -39,7 +40,7 @@ class Template implements OutputTemplateProviderInterface
      * Get all available templates for $entityType
      *
      * @param string $entityType
-     * @return array
+     * @return string[]|int[] - Collection of templateIds
      */
     public static function getTemplates(string $entityType)
     {
@@ -50,12 +51,7 @@ class Template implements OutputTemplateProviderInterface
             case self::ENTITY_TYPE_INVOICE:
             case self::ENTITY_TYPE_OFFER:
             case self::ENTITY_TYPE_DUNNING:
-                return [
-                    [
-                        'id'    => 'default',
-                        'title' => QUI::getLocale()->get('quiqqer/invoice-accounting-template', 'package.title')
-                    ]
-                ];
+                return ['system_default'];
                 break;
         }
 
@@ -63,16 +59,32 @@ class Template implements OutputTemplateProviderInterface
     }
 
     /**
+     * Get title of Template
+     *
+     * @param string|int $templateId
+     * @param Locale $Locale (optional) - If omitted use \QUI::getLocale()
+     * @return string
+     */
+    public static function getTemplateTitle($templateId, Locale $Locale = null)
+    {
+        if (empty($Locale)) {
+            $Locale = QUI::getLocale();
+        }
+
+        return $Locale->get('quiqqer/invoice-accounting-template', 'template.title');
+    }
+
+    /**
      * Get HTML for document header area
      *
-     * @param string $template
+     * @param string|int $templateId
      * @param string $entityType
      * @param EngineInterface $Engine
      * @return string|false
      *
      * @throws QUI\Exception
      */
-    public static function getHeaderHtml(string $template, string $entityType, EngineInterface $Engine)
+    public static function getHeaderHtml($templateId, string $entityType, EngineInterface $Engine)
     {
         $tplDir     = self::getTemplateDir();
         $tplTypeDir = $tplDir.$entityType.'/';
@@ -99,14 +111,14 @@ class Template implements OutputTemplateProviderInterface
     /**
      * Get HTML for document body area
      *
-     * @param string $template
+     * @param string|int $templateId
      * @param string $entityType
      * @param EngineInterface $Engine
      * @return string|false
      *
      * @throws QUI\Exception
      */
-    public static function getBodyHtml(string $template, string $entityType, EngineInterface $Engine)
+    public static function getBodyHtml($templateId, string $entityType, EngineInterface $Engine)
     {
         $tplTypeDir = self::getTemplateDir().$entityType.'/';
         $htmlFile   = $tplTypeDir.'body.html';
@@ -129,14 +141,14 @@ class Template implements OutputTemplateProviderInterface
     /**
      * Get HTML for document footer area
      *
-     * @param string $template
+     * @param string|int $templateId
      * @param string $entityType
      * @param EngineInterface $Engine
      * @return string|false
      *
      * @throws QUI\Exception
      */
-    public static function getFooterHtml(string $template, string $entityType, EngineInterface $Engine)
+    public static function getFooterHtml($templateId, string $entityType, EngineInterface $Engine)
     {
         $tplDir     = self::getTemplateDir();
         $tplTypeDir = $tplDir.$entityType.'/';
